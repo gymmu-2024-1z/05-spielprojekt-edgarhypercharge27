7744feb59a2148635f608c208c7c7808db23a7a2
Rozerin Gezer <e254561@edu.sbl.ch>
2025-04-24 20:36:27 +0000

    Start-Button wieder versucht zu reparieren

--- src/start.js
+++ src/start.js
@@ -1,5 +1,5 @@
 //Wenn man auf den Start Knopf klickt, soll das Spiel anfangen
-document.querySelector("button").addEventListener("click", () => {
+document.querySelector("#startBtn").addEventListener("click", () => {
   //Der Text am Anfang verschwindet
   document.getElementById("intro").style.display = "none"
 
